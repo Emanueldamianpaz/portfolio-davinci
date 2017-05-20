b79cbecc5c58dbcbb0c2ff74abf09ee25b258bd7
Emanuel.paz <emanuel.paz@despegar.com>
2017-05-20 20:26:08 -0300

    agregue navbar scrolleable y jquery

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    // Menu scrolleable
+    var altura = $('.topnav').offset().top;
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > altura) {
+            $('.container').addClass('menu-fixed');
+        } else {
+            $('.container').removeClass('menu-fixed');
+        }
+    });
+
+
+    $('#items a').click(function () {
+        $('#items a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+});
+
+
+
