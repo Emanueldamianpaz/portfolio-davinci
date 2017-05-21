6c279572757856d31fe88c4022f6e1065b44aa21
Emanuel.paz <emanuel.paz@despegar.com>
2017-05-21 02:40:11 -0300

    Acerca de mi terminado

--- js/script.js
+++ js/script.js
@@ -22,13 +22,15 @@ $(document).ready(function () {
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
 
-    $("#home-img").mousemove(function (e) {
+    $(".img-move").mousemove(function (e) {
         var pageX = e.pageX - ($(window).width() / 2);
         var pageY = e.pageY - ($(window).height() / 2);
         var newvalueX = width * pageX * -1 - 25;
         var newvalueY = height * pageY * -1 - 50;
-        $('#home-img').css("background-position", newvalueX + "px     " + newvalueY + "px");
+        $('.img-move').css("background-position", newvalueX + "px     " + newvalueY + "px");
     });
+
+    
 });
 
 
